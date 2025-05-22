32cd1042432435b5d411d6454caa093368d750db
Thales <thalesvicentebem10@hotmail.com>
2025-05-22 09:53:43 -0300

    This commit introduces a tab bar with three tabs to HomePage.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,9 +3,31 @@ import 'package:food_delivery/components/my_current_location.dart';
 import 'package:food_delivery/components/my_description_box.dart';
 import 'package:food_delivery/components/my_drawer.dart';
 import 'package:food_delivery/components/my_silver_app_bar.dart';
+import 'package:food_delivery/components/my_tab_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // TODO tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +37,7 @@ class HomePage extends StatelessWidget {
         headerSliverBuilder:
             (context, innerBoxIsScrolled) => [
               MySilverAppBar(
-                title: Text('title'),
+                title: MyTabBar(tabController: _tabController),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
@@ -33,7 +55,23 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ],
-        body: Container(color: Colors.blue),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("first tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("second tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("third tab items"),
+            ),
+          ],
+        ),
       ),
     );
   }
